c90f12c7c0375cb87ce73b99078f537b2ff88571
tharindulakmal98 <tharindulakmal835@gmail.com>
2025-10-26 21:44:15 +0530

    I have completed 01 task from Module10

---
+++ Module10/task04.py
@@ -0,0 +1,41 @@
+import random
+
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, speed_change):
+        self.current_speed += speed_change
+        if self.current_speed < 0:
+            self.current_speed = 0
+        elif self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
+
+class Race:
+    def __init__(self, name, distance, cars):
+        self.name = name
+        self.distance = distance
+        self.cars = cars
+
+    def hour_passes(self):
+        for car in self.cars:
+            change = random.randint(-10, 15)
+            car.accelerate(change)
+            car.drive(1)
+
+    def print_status(self):
+        for car in self.cars:
+            print(f"{car.license_plate}: speed {car.current_speed} km/h, distance {car.travelled_distance} km")
+
+    def race_finished(self):
+        for car in self.cars:
+            if car.travelled_distance >= self.distance:
+                return True
+        return False
